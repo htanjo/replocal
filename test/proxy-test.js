639f0b72531d4fee27335026f1554754cb4eadfd
Hiroyuki Tanjo <expheno@gmail.com>
2015-11-10 16:37:21 +0900

    Add test cases

--- test/proxy-test.js
+++ test/proxy-test.js
@@ -2,7 +2,24 @@
 
 var expect = require('chai').expect;
 var request = require('request');
+var path = require('path');
+var fs = require('fs');
 var Replocal = require('../lib');
+var orgWrite = process.stdout.write;
+var muteStdout = [];
+
+function mute() {
+  process.stdout.write = function (message) {
+    muteStdout.push(message);
+  };
+}
+
+function unmute() {
+  var message = muteStdout.join('');
+  muteStdout = [];
+  process.stdout.write = orgWrite;
+  return message;
+}
 
 function setup(options) {
   return new Promise(function (resolve) {
@@ -32,7 +49,7 @@ describe('Proxy server', function () {
     var replocal;
     setup({
       hostname: 'example.com',
-      docroot: 'test/fixtures/basic',
+      docroot: 'test/fixtures',
       silent: true
     })
     .then(function (proxy) {
@@ -53,6 +70,122 @@ describe('Proxy server', function () {
         done(error);
       });
     });
+  });
+
+  it('serves from cwd when no "docroot" specified', function (done) {
+    var replocal;
+    setup({
+      hostname: 'example.com',
+      silent: true
+    })
+    .then(function (proxy) {
+      replocal = proxy;
+      return requestBody({
+        url: 'http://example.com/package.json',
+        proxy: 'http://localhost:8888'
+      });
+    })
+    .then(function (body) {
+      var pkg = fs.readFileSync(path.resolve('package.json'), 'utf8');
+      expect(body).to.equal(pkg);
+    })
+    .then(function () {
+      replocal.close(done);
+    })
+    .catch(function (error) {
+      replocal.close(function () {
+        done(error);
+      });
+    });
+  });
+
+  it('listens port number specified with "port" option', function (done) {
+    var replocal;
+    setup({
+      hostname: 'example.com',
+      docroot: 'test/fixtures',
+      port: 9999,
+      silent: true
+    })
+    .then(function (proxy) {
+      replocal = proxy;
+      return requestBody({
+        url: 'http://example.com/',
+        proxy: 'http://localhost:9999'
+      });
+    })
+    .then(function (body) {
+      expect(body).to.equal('replaced');
+    })
+    .then(function () {
+      replocal.close(done);
+    })
+    .catch(function (error) {
+      replocal.close(function () {
+        done(error);
+      });
+    });
+  });
+
+  it('prints some message when start', function (done) {
+    var replocal;
+    mute();
+    setup({
+    })
+    .then(function (proxy) {
+      var message = unmute();
+      replocal = proxy;
+      expect(message).not.to.be.empty;
+    })
+    .then(function () {
+      replocal.close(done);
+    })
+    .catch(function (error) {
+      replocal.close(function () {
+        done(error);
+      });
+    });
+  });
+
+  it('doesn\'t print message when "silent" option given', function (done) {
+    var replocal;
+    mute();
+    setup({
+      silent: true
+    })
+    .then(function (proxy) {
+      var message = unmute();
+      replocal = proxy;
+      expect(message).to.be.empty;
+    })
+    .then(function () {
+      replocal.close(done);
+    })
+    .catch(function (error) {
+      replocal.close(function () {
+        done(error);
+      });
+    });
+  });
+
+  it('can close itself using .close() method', function (done) {
+    var replocal;
+    setup({
+      hostname: 'example.com',
+      docroot: 'test/fixtures',
+      silent: true
+    })
+    .then(function (proxy) {
+      replocal = proxy;
+      replocal.close();
+      request({
+        url: 'http://example.com/',
+        proxy: 'http://localhost:8888'
+      }, function (error) {
+        expect(error).not.to.be.null;
+        done();
+      });
+    })
   });
 
 });
