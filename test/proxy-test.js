8129de91b106eb94ff78ef025eac2d56a1fb26e6
Hiroyuki Tanjo <expheno@gmail.com>
2015-11-10 15:20:33 +0900

    Use native Promise object instead of Q

--- test/proxy-test.js
+++ test/proxy-test.js
@@ -2,41 +2,38 @@
 
 var expect = require('chai').expect;
 var request = require('request');
-var Q = require('q');
 var Replocal = require('../lib');
 
 function setup(options) {
-  var deferred = Q.defer();
-  var replocal = new Replocal(options);
-  replocal.on('start', function () {
-    deferred.resolve(replocal);
+  return new Promise(function (resolve) {
+    var replocal = new Replocal(options);
+    replocal.on('start', function () {
+      resolve(replocal);
+    });
   });
-  return deferred.promise;
 }
 
 function requestBody(options) {
-  var deferred = Q.defer();
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      deferred.resolve(body);
-    }
-    else {
-      deferred.reject(new Error('Could not get response'));
-    }
+  return new Promise(function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      }
+      else {
+        reject(new Error('Could not get response'));
+      }
+    });
   });
-  return deferred.promise;
 }
 
 describe('Proxy server', function () {
 
   it('replaces response body with local file', function (done) {
     var replocal;
-    Q.fcall(function () {
-      return setup({
-        hostname: 'example.com',
-        docroot: 'test/fixtures/basic',
-        silent: true
-      });
+    setup({
+      hostname: 'example.com',
+      docroot: 'test/fixtures/basic',
+      silent: true
     })
     .then(function (proxy) {
       replocal = proxy;
@@ -46,7 +43,7 @@ describe('Proxy server', function () {
       });
     })
     .then(function (body) {
-      expect(body).to.equal('replaced');
+      expect(body).to.equal('replaced?');
     })
     .then(function () {
       replocal.close(done);
@@ -55,8 +52,7 @@ describe('Proxy server', function () {
       replocal.close(function () {
         done(error);
       });
-    })
-    .done();
+    });
   });
 
 });
